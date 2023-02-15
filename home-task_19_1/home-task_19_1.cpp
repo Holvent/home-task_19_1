9af538fbd494f7eaf5576c7e9b90cabb12a955c3
Holvent ♡ <holvent@mail.ru>
2023-02-16 00:04:10 +0300

    .

--- home-task_19_1/home-task_19_1.cpp
+++ home-task_19_1/home-task_19_1.cpp
@@ -1,167 +1,64 @@
-﻿/*
-	Разработайте программу «Библиотека». Создайте структуру «Книга» (название, автор,
-	издательство, жанр). Создайте массив из 10 книг. Реализуйте для него следующие возможности:
-	- Редактировать книгу
-	- Печать всех книг
-	- Поиск книг по автору
-	- Поиск книги по названию
-	- Сортировка массива по названию книг
-	- Сортировка массива по автору
-	- Сортировка массива по издательству
-*/
-
-#include <iostream>
+﻿#include <iostream>
+#include <string>
+#include <ctime>
 using namespace std;
+
 const int SIZE = 10;
-struct Book
+struct book
 {
-	string id;
-	string title;
-	string autor;
-	string publishing;
-	string genre;
+	int id;
+	int title;
+	int autor;
+	int publishing;
+	int genre;
 
-
-	// вывод в консоль 
-	void Print(Book arrBooks)
+	void EnteringArray(book arrBooks[]);
+	void Print(book arrBooks);
+};
+// Заполнение массива для тестов чтоб не вводить каждый раз.
+void EnteringArray(book arrBooks[]) {
+	for (int i = 0; i < SIZE; i++)
 	{
-		if (arrBooks.id )
-		{
-			cout.width(7);
-			cout << "ID |" << " Название\t|" << " Автор\t\t|" << " Издательство\t|" << " Жанр\t\t|" << endl;
-			for (int i = 0; i < 73; i++)
-			{
-				cout << "-";
-			}
-			cout << endl;
-		}
+		arrBooks[i].id = (i + 1);
+		arrBooks[i].title = rand() % 10;
+		arrBooks[i].autor = rand() % 10;
+		arrBooks[i].publishing = rand() % 10;
+		arrBooks[i].genre = rand() % 10;
+	}
+}
+void Print(book arrBooks) {
+	if (arrBooks.id == 1){
+		cout << "ID |" << " Название\t|" << " Автор\t\t|" << " Издательство\t|" << " Жанр\t\t|" << endl;
+	}
+	cout << arrBooks.id << " | ";
+	cout << arrBooks.title << " | ";
+	cout << arrBooks.autor << " | ";
+	cout << arrBooks.publishing << " | ";
+	cout << arrBooks.genre << " |\n\n";
 
-		cout << arrBooks.id << " | ";
-		cout << arrBooks.title << "\t\t| ";
-		cout << arrBooks.autor << "\t\t| ";
-		cout << arrBooks.publishing << "\t\t| ";
-		cout << arrBooks.genre << "\t\t|" << endl;
+
+}
+int main() {
+	setlocale(LC_ALL, "");
+	srand(time(NULL));
+	book arrBooks[SIZE];
+	book books;
+	int numMenu = 0;
+	// Заполнение массива
+	books.EnteringArray(arrBooks);
+	do
+	{
+		cout << "\t\tМЕНЮ" << endl
+			<< "1. Печать всех книг; " << endl
+			<< "2. Редактировать книгу; " << endl
+			<< "3. Поиск книг по автору; " << endl
+			<< "4. Поиск книги по названию;" << endl
+			<< "5. Сортировка массива по названию книг;" << endl
+			<< "6. Сортировка массива по автору;" << endl
+			<< "7. Сортировка массива по издательству;" << endl
+			<< "8. Выход.\n\n";
+
+		cout << "\tВведите номер меню: ";
+		cin >> numMenu; 
 		cout << endl;
 	}
-
-	// Изменение книги
-	void EditBook(Book& book)
-	{
-		cout << "Введите название книги: "; cin >> title; cout << endl;
-		book.title = title;
-		cout << "Введите атора книги: "; cin >> autor; cout << endl;
-		book.autor = autor;
-		cout << "Введите издательство книги: "; cin >> publishing; cout << endl;
-		book.publishing = publishing;
-		cout << "Введите жанр книги: "; cin >> genre; cout << endl;
-		book.genre = genre;
-	}
-
-	//Поиск книги по автору
-	void SearchForABookByAutor(Book books[], string autor)
-	{
-		bool flag = true;
-
-		cout << "Книги с указанным автором:" << endl << endl;
-
-		for (int i = 0; i < SIZE; i++)
-		{
-			if (books[i].autor == autor)
-			{
-				if (i > 0 && i < 2)
-				{
-					cout.width(7);
-					cout << "ID |" << " Название\t|" << " Автор\t\t|" << " Издательство\t|" << " Жанр\t\t|" << endl;
-					for (int i = 0; i < 73; i++)
-					{
-						cout << "-";
-					}
-					cout << endl;
-				}
-
-				Print(books[i]);
-				flag = false;
-			}
-		}
-
-		if (flag)
-		{
-			cout << "Книги с указанным автором нет в базе." << endl;
-		}
-
-	}
-
-
-	int main()
-	{
-		setlocale(LC_ALL, "ru");
-		Book arrBooks[SIZE];
-		Book books;
-		srand(time(0));
-
-		bool yesNo = true;
-		int numMenu = 0;
-		int temp;
-		string tempStr;
-		cout << "\t\tМЕНЮ" << endl;
-		cout << "\t1. Печать всех книг; " << endl; // При необходимости можно организовать бесконечный массив данных.
-		cout << "\t2. Редактировать книгу; " << endl;
-		cout << "\t3. Поиск книг по автору; " << endl;
-		cout << "\t4. Поиск книги по названию;" << endl;
-		cout << "\t5. Сортировка массива по названию книг;" << endl;
-		cout << "\t6. Сортировка массива по автору;" << endl;
-		cout << "\t7. Сортировка массива по издательству;" << endl;
-		cout << "\t8. Выход." << endl; cout << endl;
-
-		cout << "\tВведите номер меню: "; cin >> numMenu; cout << endl;
-
-		if (numMenu == 1)
-		{
-			for (Book number : arrBooks)
-			{
-				books.Print(number);
-			}
-
-			cout << endl;
-			cout << "Нажми любую кнопку для возврата к меню.";
-			system("pause>null");
-			system("cls");
-		}
-		if (numMenu == 2)
-		{
-			cout << "Книги в базе: " << endl;
-			for (Book number : arrBooks)
-			{
-				books.Print(number);
-			}
-			cout << endl;
-
-			cout << "Введите номер id книги для изменения: "; cin >> temp;
-
-			if (temp > 0 || temp <= 10)
-			{
-				books.EditBook(arrBooks[temp - 1]);
-			}
-			else
-			{
-				cout << "Книги с таким Id нет в базе." << endl;
-			}
-
-			cout << endl;
-			cout << "Нажми любую кнопку для возврата к меню.";
-			system("pause>null");
-			system("cls");
-		}
-		if (numMenu == 3)
-		{
-			cout << "Введите автора: "; cin >> tempStr;
-
-			books.SearchForABookByAutor(arrBooks, tempStr);
-
-			cout << endl;
-			cout << "Нажми любую кнопку для возврата к меню.";
-			system("pause>null");
-			system("cls");
-		}
-	}
-};
